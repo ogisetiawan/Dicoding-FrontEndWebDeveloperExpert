98155d4cfe03c0e026a9c5d1fd280f1c1b2d1623
ogisetiawan <ogisetiawan21@gmail.com>
2023-01-30 22:06:12 +0700

    feat: mobile first - aksesbilitas content

--- module/Modules - Noted.php
+++ module/Modules - Noted.php
@@ -2,6 +2,27 @@
 /*
  * @Author: ogisetiawan21@gmail.com 
  * @Date: 2023-01-25 17:12:02 
- * @Last Modified by:   ogisetiawan21@gmail.com 
- * @Last Modified time: 2023-01-25 17:12:02 
+ * @Last Modified by: ogisetiawan21@gmail.com
+ * @Last Modified time: 2023-01-25 17:21:00
  */
+
+// @ Mobile First Approach
+//~ View port
+//? meta tag untuk ukurang beberapa device
+
+//@ Aksesbilitas
+//? untuk askes pada penyadang disabilitas
+//~ Aksesbilitas Keyboard
+//? onfokus pada sebuah form/field (tab/down/up)
+//~ Focusable element
+//? jangan gunakan element <a> untuk button ata sebliknya button untuk navigasi
+//~ Focus Order
+//? menggunakan tab-index jika dimanipulasi oleh float
+//~ ChromeVox
+//? extension chorome untuk membaca setiap element pada setiap website
+//~ Skiptocontent
+//? merupakan link tersembunyi yang dapat membuat fokus mengarah langsung ke konten utama.
+//~ Heading dan Landmarks
+//? biasanya h1 untuk kontent untama dan selanjutnya bsa dibungkus dgn section h2 - h6
+//~ Label
+//? memberikan labling pd bbrp element ( form, checkbox dll) aria-label dpt digunakan untuk labeling
