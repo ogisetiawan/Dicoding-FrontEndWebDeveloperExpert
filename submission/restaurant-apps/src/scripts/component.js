a687b2138ed7d32cd3253392a2f42edd8d55e5ff
ogisetiawan <ogisetiawan21@gmail.com>
2023-02-02 17:16:29 +0700

    feat: Event hamburger and install font-awesome

---
+++ submission/restaurant-apps/src/scripts/component.js
@@ -0,0 +1,23 @@
+import DrawerInitiator from "./drawer";
+
+class App {
+  constructor({ button, drawer, hero, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._hero = hero;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      hero: this._hero,
+      content: this._content,
+    });
+  }
+}
+
+export default App;
--- submission/restaurant-apps/src/scripts/index.js
+++ submission/restaurant-apps/src/scripts/index.js
@@ -1,5 +1,14 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
-import '../styles/media-query.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import "../styles/media-query.css";
+import '@fortawesome/fontawesome-free/js/all.js';
+import App from "./component.js";
 
-console.log('Hello Coders! :)');
+const app = new App({
+  button: document.querySelector("#hamburgerButton"),
+  drawer: document.querySelector("#navigationDrawer"),
+  hero: document.querySelector("#hero"),
+  content: document.querySelector("#mainContent"),
+});
+
+document.addEventListener('DOMContentLoaded', app);
