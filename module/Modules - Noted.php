c6059c75ec07165d43c989aba5d79ac967eb5930
ogisetiawan <ogisetiawan21@gmail.com>
2023-02-21 17:15:14 +0700

    feat: add module PWA ( AppShell )

--- module/Modules - Noted.php
+++ module/Modules - Noted.php
@@ -3,7 +3,7 @@
  * @Author: ogisetiawan21@gmail.com 
  * @Date: 2023-01-25 17:12:02 
  * @Last Modified by: ogisetiawan21@gmail.com
- * @Last Modified time: 2023-02-08 17:14:17
+ * @Last Modified time: 2023-02-21 11:34:40
  */
 
 // @ Mobile First Approach
@@ -43,3 +43,27 @@
 //? memecah satu fungsi untuk melakukan satu hal saja
 //~ Object dan Struktur data
 //? gunakan getter dan setter
+
+//@ Progressif Web App ( PWAs )
+//? PWA dibentuk agar seperti aplikasi mobile native tanpa banyak perubahan dan keahliaan khusus
+//~ Kelebihan PWA
+//? beberapa Feature native yg bsa intergrasi: gesture, notif, lokasi dll
+//? load time PWA lebih baik 
+//~ Kekurangan
+//? belum semua fitur native, karena masalah keamanan: kontak,sms,telepon dl
+//~ Komponent Pembentuk PWAs
+//# Application Shell: kerangka UI yg disimpan dahulu ke dalam cache, shgga ketika dibuka akan lbh cepat, umumnya ( header, footer yang static)
+//# Web App Manifest: file json, yg berfungsi mengunduh resource dan content 
+//# Service Worker: script latar belakang browser, yg dapat menampilkan content yg tersimpan dalam cache
+//# Cache API: penyimpanan lokal (browser) untuk simpan resource agar tidak request lg ke server
+//# Fetch API: interface standar yang digunakan browser dalam melakukan permintaan
+//# IndexedDB: penyimpanan lokal berbasis NoSQL di browser ( object JS ) peran cache
+//# Web Socket: komunikasi real time pada web ataupun aplikasi mobile.
+//# Notification: 
+//~ Web Arcitecture
+//? CSR: client side rendering ( logic di frontend )
+//? SSR: server side rendering ( logic di backend )
+//? MPA: multi page-app, setiap pindah ke menu lain browser akan memuat kembali
+//? SPA: single page-app, hanya bagian yang direquest saja yang akan di muat di browser ( meringankan di sisi server)
+//~ Appliation Shell
+//? component UI (HTML, CSS, Javascript ) akan disimpan di cache ( fokus ke data request )
