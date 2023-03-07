b0ea9cae4fba81f6a843277482503ffe151798ac
ogisetiawan <ogisetiawan21@gmail.com>
2023-03-07 11:24:01 +0700

    style: update detail restaurant

--- submission/restaurant-apps/src/scripts/views/pages/favorite.js
+++ submission/restaurant-apps/src/scripts/views/pages/favorite.js
@@ -1,21 +1,12 @@
 const Favorite = {
   async render () {
-    // return `
-    // <section class="content">
-    //   <div class="explore">
-    //     <h1 class="explore__label">Favorite Restaurant</h1>
-    //   </div>
-    //   <div class="restaurants" id="fav-restaurants">
-    //   </div>
-    // </section>
-    // `
     const hero = document.querySelector('.hero')
     hero.style.display = 'flex'
     return `
       <section class="content">
         <div class="explore">
-         <h1 class="explore__label">Your Favoritef Restaurant</h1>
-          <input id="query" type="text" class="input-resto" aria-label="search restaurant here" placeholder="Search Restaurant...">
+         <h1 class="explore__label">Your Favorite Restaurant</h1>
+          <input id="query" type="text" class="input-resto" aria-label="search restaurant here" placeholder="Search restaurant ..">
           <div id="restaurants" class="restaurants">
           </div>
         </div>
