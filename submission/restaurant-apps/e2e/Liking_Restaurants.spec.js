ff81b8d7df6367854c4af5e1fe1bf49a6a0c5a79
ogisetiawan <ogisetiawan21@gmail.com>
2023-03-16 09:58:53 +0700

    feat: add end to end with codecept

--- submission/restaurant-apps/e2e/Liking_Restaurants.spec.js
+++ submission/restaurant-apps/e2e/Liking_Restaurants.spec.js
@@ -15,16 +15,16 @@ Scenario('liking one restaurant', async ({I}) => {
   I.see('Restaurant not found', '.restaurant-item__not__found');
 
   I.amOnPage('/');
-  I.waitForElement('.restaurant__name a', 10);
+  I.waitForElement('.restaurant__name a', 5);
   I.seeElement('.restaurant__name a');
-  
 
   const firstRestaurant = locate('.restaurant__name a').first();
   const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
   I.click(firstRestaurant);
   
+  // pause();
+  I.waitForElement('#likeButton', 5); //? need adding load per seconds
   I.seeElement('#likeButton');
-  pause();
   I.click('#likeButton');
 
   I.amOnPage('/#/favorite');
@@ -32,46 +32,34 @@ Scenario('liking one restaurant', async ({I}) => {
   const likedRestaurantName = await I.grabTextFrom('.restaurant__name');
 
   assert.strictEqual(firstRestaurantName, likedRestaurantName);
-    // I.see('Restaurant not found', '.restaurant-item__not__found');
-    
-    // I.amOnPage('/');
-    // I.waitForElement('.restaurant__name a', 10);
-    
-    // I.seeElement('.restaurant__name a');
-    // I.click(locate('.restaurant__name a').first());
-
-    // pause();
-    // I.seeElement('#likeButton');
-    // I.click('#likeButton');
-
-    // I.amOnPage('/#/favorite');
-    // I.seeElement('.restaurant-item');
 });
 
-// Scenario('liking one restaurant and then unliking the restaurant', async ({I}) => {
-//   I.see('Restaurant not found', '.restaurant-item__not__found');
+Scenario('liking one restaurant and then unliking the restaurant', async ({I}) => {
+  I.see('Restaurant not found', '.restaurant-item__not__found');
 
-//   I.amOnPage('/');
+  I.amOnPage('/');
+  I.waitForElement('.restaurant__name a', 5);
+  I.seeElement('.restaurant__name a');
 
-//   I.seeElement('.restaurant__name a');
+  const firstRestaurant = locate('.restaurant__name a').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
 
-//   const firstRestaurant = locate('.restaurant__name a').first();
-//   const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
-//   I.click(firstRestaurant);
+  I.waitForElement('#likeButton', 5);
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
-//   I.seeElement('#likeButton');
-//   I.click('#likeButton');
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant-item');
+  const likedRestaurantName = await I.grabTextFrom('.restaurant__name');
 
-//   I.amOnPage('/#/favorite');
-//   I.seeElement('.restaurant-item');
-//   const likedRestaurantName = await I.grabTextFrom('.restaurant__name');
+  assert.strictEqual(firstRestaurantName, likedRestaurantName);
+  I.click(firstRestaurant);
 
-//   assert.strictEqual(firstRestaurantName, likedRestaurantName);
-//   I.click(firstRestaurant);
+  I.waitForElement('#likeButton', 5);
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
 
-//   I.seeElement('#likeButton');
-//   I.click('#likeButton');
-
-//   I.amOnPage('/#/favorite');
-//   I.see('Restaurant not found', '.restaurant-item__not__found');
-// });
+  I.amOnPage('/#/favorite');
+  I.see('Restaurant not found', '.restaurant-item__not__found');
+});
